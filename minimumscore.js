5bae2f3d7fd62642cf5567e0972c4f35a0cabdbc
Teemu Sällylä <teemu.sallyla@gmail.com>
2018-07-13 02:06:05 +0300

    Initial commit

---
+++ minimumscore.js
@@ -0,0 +1,36 @@
+
+var minScore = 0;
+
+
+function createInput(){
+    var newinput = document.createElement("input");
+    var newSpan = document.createElement("span");
+
+    newSpan.innerText = "Hide posts below score: ";
+    newinput.type = "number";
+
+    newinput.addEventListener("input", (i) => {
+        minScore = Number(i.path[0].value);
+        hideBelow();
+    });
+    
+    var spacer = Array.from(document.getElementsByClassName("content")).filter(i => i.tagName == "DIV")[0].getElementsByClassName("spacer")[0];
+    spacer.appendChild(newSpan);
+    spacer.appendChild(newinput);
+}
+
+function hideBelow(){
+    Array.from(document.getElementsByClassName("thing")).forEach(link => {
+        if (link.dataset.score){
+            if (Number(link.dataset.score) < Number(minScore)){
+                link.style.display = "none";
+            } else {
+                link.style.display = "";
+            }
+        }
+    })
+}
+
+createInput();
+
+setInterval(hideBelow, 500);
